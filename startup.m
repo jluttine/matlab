5295410fe3fc4056ebadd5d0ce4ee588c5c61f05
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2013-01-30 14:06:55 +0200

    Add a startup script

---
+++ startup.m
@@ -0,0 +1,7 @@
+
+% Set paths
+addpath(genpath('.'));
+%addpath(genpath('/home/jluttine/matlab'));
+%addpath(genpath('/share/bayes/data/jaakko'));
+%addpath(genpath('/share/climate/data'));
+%addpath(genpath('/share/climate/jluttine'));
